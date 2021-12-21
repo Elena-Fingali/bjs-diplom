585c030f6779e8ae3ea01293be0de4c60af1a940
Elena-Fingali <81174107+Elena-Fingali@users.noreply.github.com>
2021-12-21 14:18:39 +0500

    first time loginPage

---
+++ public/loginPage.js
@@ -0,0 +1,24 @@
+"use strics";
+
+let userForm = new UserForm();
+
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login(data, (response) => {
+        if (response.success === true) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage('Ошибка авторизации:' + response.error);
+        }
+    });
+};  
+    
+    userForm.registerFormCallback = (data) => {
+        ApiConnector.register(data,(response) => {
+            if(response.success === true) {
+                userForm.id = response.id;
+                location.reload();
+            } else {
+        userForm.setRegisterErrorMessage('Ошибка регистрации:' + response.error);
+    }
+  });
+};
